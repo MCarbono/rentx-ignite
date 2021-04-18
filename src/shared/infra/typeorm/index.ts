b62b69936159dbe59c96aa74e6107ecfc964b3ff
Marcelo <carbono.marcelo@gmail.com>
2021-04-18 12:29:34 -0300

    database connection changes

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -12,11 +12,10 @@ getConnectionOptions().then((options) => {
     })
 })*/
 
-export default async(host = 'localhost'): Promise<Connection> => {
+export default async(): Promise<Connection> => {
     const defaultOptions = await getConnectionOptions()
     return createConnection(
         Object.assign(defaultOptions, {
-            host: process.env.NODE_ENV === "test" ? "localhost" : host,
             database: process.env.NODE_ENV === "test" ? "rentx_test" : defaultOptions.database
         })
     )
